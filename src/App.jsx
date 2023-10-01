9ba7c5b8e07119637758d6c776db76c90f1f3565
Shivam Modi <modishivam037@gmail.com>
2023-10-01 19:33:38 +0530

    Header updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/sing-in" element={<SignIn />} />
+      <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/about" element={<About />} />
