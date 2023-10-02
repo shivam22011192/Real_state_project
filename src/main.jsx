4af714822891e4a5cc737d15eeddbe65c7929aa7
Shivam Modi <modishivam037@gmail.com>
2023-10-02 19:33:14 +0530

    add redux tool kit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import {store} from './redux/store.js';
+import {provider} from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <provider store={store}>
     <App />
-  </React.StrictMode>,
+  </provider>,
 )
