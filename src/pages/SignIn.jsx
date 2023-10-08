5e7b5eb9b40f914535c47c4af9ebfaa11623df8d
Shivam Modi <modishivam037@gmail.com>
2023-10-08 13:12:56 +0530

    added Sign Out Functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
-import {signInState, signInSuccess, signInFailure} from '../redux/user/userSlice';
+import {signInStart, signInSuccess, signInFailure} from '../redux/user/userSlice';
 import OAuth from '../components/OAuth';
 
 
@@ -19,7 +19,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      dispatch(signInState());
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
