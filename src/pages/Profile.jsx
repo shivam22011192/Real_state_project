7537573dd64c238c074ce5c714b5cdcbad39a2e8
Shivam Modi <modishivam037@gmail.com>
2023-10-15 21:05:13 +0530

    completed User Listing Delete func

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -141,6 +141,24 @@ export default function Profile() {
       setShowListingsError(true);
     }
   };
+  const handleListingDelete= async(listingID)=>{
+    try {
+      const res = await fetch(`/api/listing/delete/${listingID}`,{
+        method:'DELETE',
+      });
+      const data= await res.json();
+      if(data.success ===false){
+        console.log(data.message);
+        return;
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+    setUserListings((prev)=>prev.filter((listing)=>listing._id !== listingID));
+
+  }
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -254,7 +272,7 @@ export default function Profile() {
               </Link>
   
               <div className='flex flex-col item-center'>
-                <button className='text-red-700 uppercase'>Delete</button>
+                <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
                 <button className='text-green-700 uppercase'>Edit</button>
               </div>
             </div>
