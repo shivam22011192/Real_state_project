1575add19084ede608fac04469182a45dc1403bb
Shivam Modi <modishivam037@gmail.com>
2023-10-16 12:53:06 +0530

    added contact Landlord Func and Mail servicer to the Createlisting page

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/about" element={<About />} />
-      <Route path="/listing/:listingId " element={<Listing />} />
+      <Route path="/listing/:listingId" element={<Listing />} />
       <Route element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />
         <Route path="/create-listing" element={<CreateListing />} />
